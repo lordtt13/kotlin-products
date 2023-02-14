7a837c0b48a48f531e41e9e937ae5687149e7fd3
lordtt13 <tanmaythakurbrn2rule@gmail.com>
2023-02-15 02:40:24 +0530

    feat: add application module
    Signed-off-by: lordtt13 <tanmaythakurbrn2rule@gmail.com>

---
+++ chat-with-sockets/src/main/kotlin/com/tt/Application.kt
@@ -0,0 +1,13 @@
+package com.tt
+
+import io.ktor.server.application.*
+import com.tt.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureSockets()
+    configureRouting()
+}
