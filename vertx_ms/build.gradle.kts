f4bdebae2710902a286847d52393db7e45e2944e
lordtt13 <tanmaythakurbrn2rule@gmail.com>
2023-02-15 02:59:21 +0530

    feat: add build info for vertx ms
    Signed-off-by: lordtt13 <tanmaythakurbrn2rule@gmail.com>

---
+++ vertx_ms/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    kotlin("jvm") version "1.8.0"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+val vertxVersion = "4.1.5"
+dependencies {
+    implementation("io.vertx:vertx-core:$vertxVersion")
+    implementation("io.vertx:vertx-web:$vertxVersion")
+    implementation("io.vertx:vertx-web-client:$vertxVersion")
+    implementation("io.vertx:vertx-lang-kotlin:$vertxVersion")
+    implementation("io.vertx:vertx-lang-kotlin-coroutines:$vertxVersion")
+    implementation("io.vertx:vertx-pg-client:$vertxVersion")
+    implementation("org.postgresql:postgresql:42.3.0")
+    implementation("com.ongres.scram:common:2.1")
+    implementation("com.ongres.scram:client:2.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
